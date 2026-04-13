91c16f128cb12443dc8e182b405fc6a8f07b5b08
RomainWirth <romain.w@gloobles.com>
2026-04-13 16:31:03 +0200

    update programm

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"example.com/price-calculator/cmdmanager"
+	"fmt"
+
+	"example.com/price-calculator/filemanager"
 	"example.com/price-calculator/prices"
 )
 
@@ -9,9 +11,12 @@ func main() {
 	taxRates := []float64{0, 0.021, 0.055, 0.1, 0.2}
 
 	for _, taxRate := range taxRates {
-		// fm := filemanager.New("./data/prices.txt", fmt.Sprintf("./results_json/tax_included_prices_%.2f.json", taxRate*100))
-		cmdm := cmdmanager.New()
-		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
-		priceJob.Process()
+		fm := filemanager.New("./data/prices.txt", fmt.Sprintf("./results_json/tax_included_prices_%.2f.json", taxRate*100))
+		// cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		err := priceJob.Process()
+		if err != nil {
+			fmt.Println("Erreur lors du traitement des prix : ", err)
+		}
 	}
 }
