2757e5042d471e1566ac3a568ef76c916473bfda
RomainWirth <romain.w@gloobles.com>
2026-04-14 15:47:06 +0200

    ADD error channel

--- main.go
+++ main.go
@@ -10,20 +10,29 @@ import (
 func main() {
 	taxRates := []float64{0, 0.021, 0.055, 0.1, 0.2}
 	doneChans := make([]chan bool, len(taxRates))
+	errorChans := make([]chan error, len(taxRates))
 
 	for i, taxRate := range taxRates {
 		doneChans[i] = make(chan bool)
+		errorChans[i] = make(chan error)
 		fm := filemanager.New("./data/prices.txt", fmt.Sprintf("./results_json/tax_included_prices_%.2f.json", taxRate*100))
 		// cmdm := cmdmanager.New()
 		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
-		go priceJob.Process(doneChans[i])
+		go priceJob.Process(doneChans[i], errorChans[i])
 
 		// if err != nil {
 		// 	fmt.Println("Erreur lors du traitement des prix : ", err)
 		// }
 	}
 
-	for _, doneChan := range doneChans {
-		<-doneChan
+	for i := range taxRates {
+		select {
+		case err := <-errorChans[i]:
+			if err != nil {
+				fmt.Println(err)
+			}
+		case <-doneChans[i]:
+			fmt.Println("Done !")
+		}
 	}
 }
